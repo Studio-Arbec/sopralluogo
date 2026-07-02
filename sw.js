c59591f43c5888b7e2634ce8021825fcbc3de1cf
CesareMario2022 <130856607+CesareMario2022@users.noreply.github.com>
2026-07-02 15:43:42 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+/* Service worker - cache per uso offline */
+const CACHE = 'sopralluogo-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request).then((resp) => {
+        if (e.request.method === 'GET' && resp.ok) {
+          const copy = resp.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+        }
+        return resp;
+      }).catch(() => cached);
+    })
+  );
+});
